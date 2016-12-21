747616a4a2e12389363e5bbf43d8f29a990bac36
Clint Singer <clint@singerfmaily.ca>
2016-12-21 14:12:01 -0800

    Exports AuthenticationContext

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// export * from './adal.authentication';
-// export * from './adal.authentication.context';
 export * from './authentication';
+export * from './authentication.context';
 export * from './adal.config';
